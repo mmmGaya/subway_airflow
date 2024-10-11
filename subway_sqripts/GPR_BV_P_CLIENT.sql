1137cf02c1d26b265de429fa70fd8bd4e4fed276
bbhkls <kimliserp@gmail.com>
2024-10-11 12:40:14 +0300

    modif condition  in PIT table

--- subway_sqripts/GPR_BV_P_CLIENT.sql
+++ subway_sqripts/GPR_BV_P_CLIENT.sql
@@ -38,11 +38,11 @@ where (client_rk, valid_from_dttm) in (select ac.client_rk a, max(valid_from_dtt
 
 update dbt_schema."GPR_BV_P_CLIENT"								
 set (dataflow_id, dataflow_dttm, valid_to_dttm) = (select run_id, execution_date, execution_date from dbt_schema.metadata_airflow)
-where (client_rk, valid_from_dttm, 1) in (select ac.client_rk a, max(valid_from_dttm), min(delete_flg)
+where ((client_rk, valid_from_dttm, 1) in (select ac.client_rk a, max(valid_from_dttm), min(delete_flg)
 										from dbt_schema."GPR_RV_S_CLIENT" sc 
 										join dbt_schema."GPR_BV_A_CLIENT" ac on sc.client_rk = ac.x_client_rk 
 										group by ac.client_rk)
-   or client_rk not in (select client_rk from dbt_schema."GPR_BV_A_CLIENT");
+   or client_rk not in (select client_rk from dbt_schema."GPR_BV_A_CLIENT")) and extract(year from valid_to_dttm) = 5999;
 
 -- вставка записей о новых экземпляров
 insert into dbt_schema."GPR_BV_P_CLIENT"					   	  

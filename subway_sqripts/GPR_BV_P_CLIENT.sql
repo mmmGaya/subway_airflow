8d304e3a689f3e683b59f9dde37035e25b9af33d
bbhkls <kimliserp@gmail.com>
2024-10-11 12:34:25 +0300

    modif info about flag in PIT table

--- subway_sqripts/GPR_BV_P_CLIENT.sql
+++ subway_sqripts/GPR_BV_P_CLIENT.sql
@@ -28,11 +28,19 @@ where (client_rk, valid_from_dttm) in (select client_rk, valid_from_dttm
 											where valid_to_dttm <> ld);
 
 -- обновление конца периода в случае, когда экземпляр сущности удален
-update dbt_schema."GPR_BV_P_CLIENT"								
+/* update dbt_schema."GPR_BV_P_CLIENT"								
 set (dataflow_id, dataflow_dttm, valid_to_dttm) = (select run_id, execution_date, execution_date from dbt_schema.metadata_airflow)
 where (client_rk, valid_from_dttm) in (select ac.client_rk a, max(valid_from_dttm)
 										from dbt_schema."GPR_RV_S_CLIENT" sc 
 										join dbt_schema."GPR_BV_A_CLIENT" ac on sc.client_rk = ac.x_client_rk and (delete_flg = 1 or actual_flg = 0)
+										group by ac.client_rk)
+   or client_rk not in (select client_rk from dbt_schema."GPR_BV_A_CLIENT"); */
+
+update dbt_schema."GPR_BV_P_CLIENT"								
+set (dataflow_id, dataflow_dttm, valid_to_dttm) = (select run_id, execution_date, execution_date from dbt_schema.metadata_airflow)
+where (client_rk, valid_from_dttm, 1) in (select ac.client_rk a, max(valid_from_dttm), min(delete_flg)
+										from dbt_schema."GPR_RV_S_CLIENT" sc 
+										join dbt_schema."GPR_BV_A_CLIENT" ac on sc.client_rk = ac.x_client_rk 
 										group by ac.client_rk)
    or client_rk not in (select client_rk from dbt_schema."GPR_BV_A_CLIENT");
 
